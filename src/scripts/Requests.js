387405540e1dd291579aa695ecfcdce3bc0a7bfe
ts <shanksleslietrey@gmail.com>
2021-08-04 15:53:16 -0500

    first commit

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -49,8 +49,7 @@ export const Requests = () => {
                             ).join("")
                         }
                     </select>
-                </li>`
-    
+                </li>`   
             }
         )
     html += requestsArray.join("")
